3f0987c83839be6ef6481b2a489ae4d05d7e5d8f
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-06-29 03:38:53 -0700

    hero section

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+console.log("✅ script.js is working");
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
 
-  const menuBtn = document.getElementById("menu-btn");
-  const mobileMenu = document.getElementById("mobile-menu");
-
-  menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
-  });
+});
+function changeImage(index) {
+    const heroImg = document.querySelector('#hero img');
+    if (!heroImg) {
+        console.log("Image not found.");
+        return;
+    }
+
+    if (index === 1) {
+        heroImg.src = "Images/HeroSection_1.jpg";
+        title.textContent = "Model 3";
+        subtitle.textContent = "0% APR Ending June 30";
+    } else if (index === 2) {
+        heroImg.src = "Images/HeroSection_2.jpg";
+        title.textContent = "Model Y";
+        subtitle.textContent = "Eligible for Tax Credit";
+    }
+}
